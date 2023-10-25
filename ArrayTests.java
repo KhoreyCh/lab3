a5e83c816228cf391d6de0e99d3817cb03a27f7e
KhoreyCh <knchahal@ucsd.edu>
2023-10-25 16:42:24 -0700

    bug fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,6 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace5Inputs() {
+    int[] input1 = { 3,4,5,6 };
+    int[] input2 = {6,5,4,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(input2, input1);
+	}
+
+  @Test 
+	public void testReversedInputs() {
+    int[] input1 = { 3,4,5,6 };
+    int[] input2 = {6,5,4,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(input2, input1);
+	}
+
 
   @Test
   public void testReversed() {
