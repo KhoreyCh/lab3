175ddc32ed8309908d5115b42bffad15ff2d7920
KhoreyCh <knchahal@ucsd.edu>
2023-10-25 17:49:17 -0700

    More bugs

--- ListTest.java
+++ ListTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
 import java.util.ArrayList;
 
 import org.junit.*;
@@ -18,6 +19,25 @@ public class ListTest {
         myTest.add("bye");
         List<String> result = tester.filter(myTest,test);
         assertEquals("hi",result.get(0));
+    }
+
+    @Test
+    public void mergTest()
+    {
+        ArrayList<String> merg1 = new ArrayList<String>();
+        ArrayList<String> merge2 = new ArrayList<String>();
+
+        merg1.add("a");
+        merg1.add("a");
+        merg1.add("b");
+        merg1.add("c");
+
+        merge2.add("a");
+        merge2.add("a");
+        merge2.add("a");
+        merge2.add("b");
+
+        List<String> result = ListExamples.merge(merg1, merge2);
     }
     
 }
